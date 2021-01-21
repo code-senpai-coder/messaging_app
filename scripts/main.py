694cbd8e9d9ce7b9837ae567d54523a54a43e8ac
code-senpai-coder <65922483+code-senpai-coder@users.noreply.github.com>
2021-01-21 14:46:39 +0100

    added screen info getiing

--- scripts/main.py
+++ scripts/main.py
@@ -6,6 +6,7 @@ import logout_script
 import message_reader
 import send_message
 import time
+import datetime
 
 print("Welcome to the messaging app: \n")
 #Main loop function
@@ -25,5 +26,16 @@ def main():
     user_logged_info = open("data\logged_user_info.txt", "r+")
     logged_name = user_logged_info.readline()
     user_logged_info.close()
+
+    #getting everything to display on screen 
+    screen = ""
+    screen += f"logged as: {logged_name} \n"
+    screen += f"time: {datetime.datetime.now()} \n"
+    messages = message_reader.readMessage()
+    for text in messages:
+        screen += text 
+        screen += "\n"
+        
+    print(screen)
 
 main()
